d4ba685794af1dec490f97cf0395254849c9a494
Doug Warren <git@dpw67.oib.com>
2011-05-08 18:52:04 -0500

    Lock down specific projects controller actions for admins only.  Still about 15 Cucumber failures unresolved.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,14 @@
 module ApplicationHelper 
+  
   def title(*parts)
-    @title = (parts << "Ticketee").join(" - ") unless parts.empty?
+    @title = (parts << "Ticketee").join(" - ") unless parts.empty? 
     @title || "Ticketee" 
   end
+  
+  def admins_only(&block)
+    block.call if current_user.try(:admin?)
+    nil 
+  end
+  
 end
 
